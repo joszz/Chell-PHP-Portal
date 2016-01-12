9b923c390b89b1f6c899608f9c19f4f15d9d0c47
Jos Nienhuis <jos_nienhuis@hotmail.com>
2016-01-12 12:09:44 +0100

    Forgot to include controller changes for JS settings

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -4,8 +4,12 @@ class IndexController extends BaseController
 {
     public function indexAction()
     {
-        
         $this->view->disks = Diskdrives::DiskStatisticsLocal();
         $this->view->devices = Devices::find(array('order' => 'name ASC'));
+    }
+
+    public function dashboardSettingsAction()
+    {
+        die(json_encode($this->config->dashboard));
     }
 }
