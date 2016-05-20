0398cbea2197fd814b3ed021f2f3a7ba49e0102e
Jos Nienhuis <jos_nienhuis@hotmail.com>
2016-05-20 13:30:30 +0200

    Fix for issue #9
    Fading in nav-tabs and tab-content caused issues with the tab collapse
    plugin.

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,12 +1,10 @@
 ﻿$(function () {
+    $('.nav-tabs').tabCollapse();
+
     //Set focus to correct tab when URL navigated to with location.hash
     if (location.hash) {
         $("a[href='" + location.hash + "']").tab("show");
     }
-
-    //This prevents weird jumpiness when a location.hash is present and is switched on document load (see above)
-    $("ul.nav-tabs").fadeIn();
-    $("div.tab-content").fadeIn();
 
     $(".nav-tabs a").click(function () {
         location.hash = $(this).attr("href");
@@ -15,6 +13,4 @@
     $("form a.glyphicon-remove").click(function () {
         return (confirm("Are you sure you want to delete this item?"));
     });
-
-    $('.nav-tabs').tabCollapse();
 });
