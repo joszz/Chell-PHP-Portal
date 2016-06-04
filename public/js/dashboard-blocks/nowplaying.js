1f8a301ed3e2ab6f109b73b4ec463369860a120b
Jos Nienhuis <jos_nienhuis@hotmail.com>
2016-06-04 08:50:51 +0200

    WIP Now playing block
    - Refactored
    - Can now switch between Kodi and SubSonic
    - added nowPlayingCallback called for both subsonic and kodi to handle
    nothing playing, only one player playing and setting title etc.
    - lots more

--- public/js/dashboard-blocks/nowplaying.js
+++ public/js/dashboard-blocks/nowplaying.js
@@ -3,11 +3,13 @@
         var settings = $.extend({
             block: $(this),
             subsonic: {
+                loading: false,
                 url: $(this).data("subsonic-url"),
                 password: $(this).data("subsonic-password"),
                 username: $(this).data("subsonic-username"),
             },
             kodi: {
+                loading: false,
                 url: $(this).data("kodi-url"),
                 urlJSON: $(this).data("kodi-url") + "/jsonrpc",
                 password: $(this).data("kodi-password"),
@@ -15,29 +17,88 @@
             },
         }, options);
 
-        settings.block.find(".glyphicon-refresh").off().on("click", function () {
-            functions.subsonic.nowPlaying();
+        settings.block.find(".glyphicon-refresh").click(function () {
+            functions.nowPlaying();
         });
 
         settings.block.find(".glyphicon-chevron-left, .glyphicon-chevron-right").click(function () {
-            functions.subsonic.rotate($(this).hasClass("glyphicon-chevron-left") ? "left" : "right");
+            functions.rotate($(this).hasClass("glyphicon-chevron-left") ? "left" : "right");
         });
 
         var functions = {
-            subsonic: {
-                nowPlaying: function (onload) {
-                    onload = typeof onload === 'undefined' ? false : onload;
+            nowPlaying: function (onload) {
+                onload = typeof onload === 'undefined' ? false : onload;
 
-                    if (!onload) {
-                        $(".nowplaying").isLoading({
-                            text: "Loading",
-                            position: "overlay"
-                        });
+                if (!onload) {
+                    $(".nowplaying").isLoading({
+                        text: "Loading",
+                        position: "overlay"
+                    });
+                }
+
+                functions.subsonic.nowPlaying();
+                functions.kodi.nowPlaying();
+
+                if (!onload) {
+                    $(".nowplaying").isLoading("hide");
+                }
+            },
+
+            nowPlayingCallback: function () {
+                if (settings.kodi.loading == false && settings.subsonic.loading == false) {
+                    var playerCount = settings.block.find(".player:not(.hidden)").length;
+
+                    if (playerCount == 0) {
+                        var clone = settings.block.find(".player.hidden").clone();
+
+                        clone.find(".item").css({ "background-image": "url(img/icons/unknown.jpg)" }).addClass("disabled");
+                        clone.find(".title").html("Nothing playing");
+                        clone.removeClass("hidden");
+
+                        clone.appendTo(settings.block.find(".panel-body"));
                     }
+                    else if (playerCount == 1) {
+                        settings.block.find(".panel-heading button:not(.glyphicon-refresh)").addClass("disabled");
+                    }
+                    else {
+                        settings.block.find(".panel-heading button:not(.glyphicon-refresh)").removeClass("disabled");
+                    }
+
+                    var title = settings.block.find(".player:visible").hasClass("kodi") ? "Kodi" : "Subsonic";
+                    settings.block.find("h4").html(title);
+                }
+            },
+
+            rotate: function (direction) {
+                var currentIndex = settings.block.find(".player:visible").index();
+                var offset = direction == "right" ? 1 : -1;
+                var nextIndex = 1;
+                var nextBlock = settings.block.find(".player:eq(" + (currentIndex + offset) + ")");
+
+                if (nextBlock.length == 1 && !nextBlock.hasClass("hidden")) {
+                    nextIndex = currentIndex + offset;
+                }
+                else if (nextBlock.hasClass("hidden")) {
+                    nextIndex = settings.block.find(".player").length - 1;
+                }
+
+                if (currentIndex != nextIndex) {
+                    settings.block.find(".player:eq(" + currentIndex + ")").fadeOut("fast", function () {
+                        settings.block.find(".player:eq(" + nextIndex + ")").fadeIn("fast", function () {
+                            var title = settings.block.find(".player:visible").hasClass("kodi") ? "Kodi" : "Subsonic";
+                            settings.block.find("h4").html(title);
+                        }).css("display", "block");
+                    });
+                }
+            },
+
+            subsonic: {
+                nowPlaying: function () {
+                    settings.subsonic.loading = true;
 
                     $.ajax({
                         url: functions.subsonic.getURL("getNowPlaying"),
-                        //todo: foreach entry
+
                         success: function (nowPlayingData) {
                             var entry = $(nowPlayingData).find("entry");
 
@@ -52,7 +113,9 @@
                                     var duration = Math.floor($(this).attr("duration") / 60) + ":" + ($(this).attr("duration") % 60);
 
                                     clone.removeClass("hidden");
-                                    if (index > 0) {
+                                    clone.addClass("subsonic");
+
+                                    if (settings.block.find(".player:visible").length != 0) {
                                         clone.addClass("hidden_not_important");
                                     }
 
@@ -76,27 +139,12 @@
                                     clone.appendTo(settings.block.find(".panel-body"));
                                 });
                             }
-                            /* todo: keep in mind Kodi items
-                            else {
-                                var clone = settings.block.find(".player.hidden").clone();
 
-                                clone.find(".item").css({ "background-image": "url(img/icons/unknown.jpg)" }).addClass("disabled");
-                                clone.find(".title").html("Nothing playing");
-                                clone.removeClass("hidden");
+                            settings.subsonic.loading = false;
 
-                                clone.appendTo(settings.block.find(".panel-body"));
-                            }
-
-                            if (entry.length == 1) {
-                                settings.block.find(".panel-heading button:not(.glyphicon-refresh)").addClass("disabled");
-                            }
-                            */
+                            functions.nowPlayingCallback();
                         }
                     });
-
-                    if (!onload) {
-                        $(".nowplaying").isLoading("hide");
-                    }
                 },
 
                 getSalt: function () {
@@ -114,30 +162,12 @@
 
                     return url;
                 },
-
-                rotate: function (direction) {
-                    var currentIndex = settings.block.find(".player:visible").index();
-                    var offset = direction == "right" ? 1 : -1;
-                    var nextIndex = 1;
-                    var nextBlock = settings.block.find(".player:eq(" + (currentIndex + offset) + ")");
-
-                    if (nextBlock.length == 1 && !nextBlock.hasClass("hidden")) {
-                        nextIndex = currentIndex + offset;
-                    }
-                    else if (nextBlock.hasClass("hidden")) {
-                        nextIndex = settings.block.find(".player").length - 1;
-                    }
-
-                    if (currentIndex != nextIndex) {
-                        settings.block.find(".player:eq(" + currentIndex + ")").fadeOut("fast", function () {
-                            settings.block.find(".player:eq(" + nextIndex + ")").fadeIn("fast").css("display", "block");
-                        });
-                    }
-                }
             },
 
             kodi: {
                 nowPlaying: function () {
+                    settings.kodi.loading = true;
+
                     var data = {
                         id: 1,
                         jsonrpc: "2.0",
@@ -154,26 +184,37 @@
                             xhr.setRequestHeader('Authorization', 'Basic ' + btoa(settings.kodi.username + ':' + settings.kodi.password));
                         },
                         success: function (response) {
-                            var clone = settings.block.find(".player.hidden").clone();
-                            var image = encodeURI(response.result.item.thumbnail);
-                            clone.removeClass("hidden");
-                            
-                            clone.find(".item").css("background-image", "url(" + settings.kodi.url + "/image/" + image + ")")
-                                     .attr({ title: $(this).attr("title") })
-                                     .removeClass("disabled");
+                            if ($.trim(response.result.item.title) != "") {
+                                var clone = settings.block.find(".player.hidden").clone();
+                                var imageURL = encodeURI(response.result.item.thumbnail);
+                                imageURL = settings.kodi.url.replace("//", "//" + settings.kodi.username + ":" + settings.kodi.password + "@") + "/image/" + imageURL;
 
-                            clone.find(".title").html(response.result.item.artist);
-                            clone.find(".subtitle").html(response.result.item.title);
+                                clone.removeClass("hidden");
+                                clone.addClass("kodi");
 
-                            clone.appendTo(settings.block.find(".panel-body"));
+                                if (settings.block.find(".player:visible").length != 0) {
+                                    clone.addClass("hidden_not_important");
+                                }
+
+                                clone.find(".item").css("background-image", "url(" + imageURL + ")")
+                                         .attr({ title: $(this).attr("title") })
+                                         .removeClass("disabled");
+
+                                clone.find(".title").html(response.result.item.artist);
+                                clone.find(".subtitle").html(response.result.item.title);
+
+                                clone.appendTo(settings.block.find(".panel-body"));
+                            }
+                            settings.kodi.loading = false;
+
+                            functions.nowPlayingCallback();
                         }
                     });
                 },
             }
         };
 
-        functions.subsonic.nowPlaying(true);
-        functions.kodi.nowPlaying();
+        functions.nowPlaying(true);
 
         return functions;
     }
