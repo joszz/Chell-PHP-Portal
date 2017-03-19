0487a0b27e6e6b3c125ef5a9fa60e0c3261ade3a
Jos Nienhuis <jos_nienhuis@hotmail.com>
2017-03-19 22:12:30 +0100

    Small styling changes in settings

--- app/forms/SettingsUsersForm.php
+++ app/forms/SettingsUsersForm.php
@@ -7,14 +7,31 @@ use Phalcon\Validation\Validator\PresenceOf;
 
 /**
  * The form responsible for updating existing Users.
- * 
+ *
  * @package Forms
  */
-class SettingsUsersForm
+class SettingsUsersForm extends Form
 {
     /**
+     * The configuration object containing all the info from config.ini.
+     * @var array
+     */
+    private $_config;
+
+    /**
+     * Set the config array (config.ini contents) to private variable.
+     *
+     * @param array $config     The config array.
+     */
+    public function __construct($config)
+    {
+        $this->_config = $config;
+        parent::__construct();
+    }
+
+    /**
      * Loops over all users and sets fields for each user
-	 * 
+	 *
 	 * @param array $users	All users currently stored in the database.
      */
     public function initialize($users)
