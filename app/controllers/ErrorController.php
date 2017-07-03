9e1176925e558a867cd2e26114da1f67c305e83b
Jos Nienhuis <jos_nienhuis@hotmail.com>
2017-07-03 16:00:35 +0200

    Disable caching of error page

--- app/controllers/ErrorController.php
+++ app/controllers/ErrorController.php
@@ -46,6 +46,9 @@ class ErrorController
 
         $this->writeLogAsHTML($exceptionContent);
 
+        header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
+        header('Cache-Control: post-check=0, pre-check=0', false);
+        header('Pragma: no-cache');
 
         die($this->content);
     }
