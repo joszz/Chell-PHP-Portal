bd2d89d256985c0412952d5a6b499da70724a2ab
Jos Nienhuis <jos_nienhuis@hotmail.com>
2017-10-15 12:35:53 +0200

    WIP

---
+++ public/js/dashboard-blocks/hyperv-admin.js
@@ -0,0 +1,45 @@
+﻿"use strict";
+
+/**
+* The devices block on the dashboard.
+* 
+* @class Devices
+* @module Dashboard
+* @submodule DashboardBlocks
+*/
+(function ($) {
+    $.fn.hypervadmin = function (options) {
+
+        /**
+        * All the settings for this block.
+        * 
+        * @property settings
+        * @type Object
+        */
+        var settings = $.extend({
+            block: this,
+        }, options);
+
+        /**
+        * All the functions for this block.
+        * 
+        * @property functions
+        * @type Object
+        */
+        var functions = {
+
+            /**
+            * Initializes the eventhandlers for the various button clicks.
+            * 
+            * @method checkstates
+            */
+            initialize: function () {
+  
+            },
+        };
+
+        functions.initialize();
+
+        return functions;
+    };
+})(jQuery);
