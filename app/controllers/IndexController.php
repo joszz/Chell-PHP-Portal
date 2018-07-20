a2be15ece29162f9c94042a243fad0916a4e0679
Jos Nienhuis <jos_nienhuis@hotmail.com>
2018-07-20 13:44:45 +0200

    Add link rel DNS prefetch
    Parsing the config and setting DNS prefetch for all plugins enabled which aren't hosted in the current domain (also subdomains need to be prefetched).
    Also prefetch the TMDBApi DNS entry

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -25,6 +25,8 @@ class IndexController extends BaseController
      */
     public function indexAction()
     {
+        $this->view->dnsPrefetchRecords = $this->setDNSPrefetchRecords();
+
         $this->view->devices = Devices::find(array('order' => 'name ASC'));
         $this->view->movies = KodiMovies::getLatestMovies();
         $this->view->albums = KodiMusic::getLatestAlbums();
@@ -43,5 +45,40 @@ class IndexController extends BaseController
     {
         header('Content-Type: application/manifest+json; charset=UTF-8');
         $this->view->setRenderLevel(View::LEVEL_ACTION_VIEW);
+    }
+
+    /**
+     * Sets the URLs for <link rel="dns-prefetch" />
+     * 
+     * @return array An array with strings for the DNS prefetch URLs
+     */
+    private function setDNSPrefetchRecords()
+    {
+        $hostname = getenv('HTTP_HOST');
+        $dnsPrefetchRecords = array($this->config->application->tmdbAPIURL);
+
+        foreach($this->config as $configSectionValue)
+        {
+            foreach($configSectionValue as $configKey => $configValue)
+            {
+                // Disabled in the config so skip complete section
+                if(strtolower($configKey) == 'enabled' && !$configValue)
+                {
+                    continue 2;
+                }
+
+                if(strtolower($configKey) == 'url')
+                {
+                    $parsedURL = parse_url($configValue);
+
+                    if ($parsedURL['host'] != $hostname)
+                    {
+                        $dnsPrefetchRecords[] = $parsedURL['scheme'] . '://' . $parsedURL['host'];
+                    }
+                }
+            }
+        }
+
+        return $dnsPrefetchRecords;
     }
 }
