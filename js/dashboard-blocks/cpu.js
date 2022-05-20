0a57a4067285392db75526e69d80ab64044d8a72
Jos Nienhuis <jos_nienhuis@hotmail.com>
2022-05-20 23:02:51 +0200

    Update cpu.js

--- js/dashboard-blocks/cpu.js
+++ js/dashboard-blocks/cpu.js
@@ -34,6 +34,9 @@
                     plugins: {
                         legend: {
                             display: false,
+                        },
+                        tooltip: {
+                            enabled: false
                         }
                     },
                     elements: {
