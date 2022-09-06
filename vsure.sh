c8984276a33ece23b019939ed22132e59afabf27
Jos Nienhuis <jos_nienhuis@hotmail.com>
2022-09-06 16:18:42 +0200

    Add versisure MFA script

---
+++ vsure.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+# Run as:
+# docker run -it --user "www-data:www-data" <containername/containerid> /var/www/portal/vsure.sh
+
+USERNAME=""
+PASSWORD=""
+
+read -p "Verisure username: " USERNAME
+read -p "Verisure password: " PASSWORD
+
+vsure $USERNAME $PASSWORD mfa
