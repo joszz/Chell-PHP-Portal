554abd5100518f001716e393be2798b138231d03
Jos Nienhuis <jos_nienhuis@hotmail.com>
2023-01-30 15:10:14 +0100

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 
-envsubst '${SERVERNAME}' < /etc/nginx/http.d/default.conf.template > /etc/nginx/http.d/default.conf
-envsubst '${CHELLPORT}' < /etc/nginx/http.d/default.conf.template > /etc/nginx/http.d/default.conf
+envsubst '${SERVERNAME} ${CHELLPORT}' < /etc/nginx/http.d/default.conf.template > /etc/nginx/http.d/default.conf
 
 nginx
 crond
